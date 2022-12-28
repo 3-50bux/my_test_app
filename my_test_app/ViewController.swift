acb15e401b31a41c45a88edbc0a5868f0ec5450e
gialo <idybaieys@gmail.com>
2022-12-28 07:09:30 +0400

    Initial Commit

---
+++ my_test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_test_app
+//
+//  Created by user on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
