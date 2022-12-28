7a561830a406e447f0509961735d85dfaa392c82
gialo <idybaieys@gmail.com>
2022-12-28 08:54:21 +0400

    add action

--- my_test_app/ViewController.swift
+++ my_test_app/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var hideLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        hideLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func toggleButton(_ sender: Any) {
+        if hideLabel.isHidden{
+            hideLabel.isHidden = false
+            toggleButton.setTitle("HIDE", for: .normal)
+          
+        }else{
+            hideLabel.isHidden = true
+            toggleButton.setTitle("SHOW", for: .normal)
+          
+        }
+    }
+    
 }
-
